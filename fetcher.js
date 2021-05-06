05483a5cfd50fd0e0b63828c599b3ab929412a65
Zio <zi.ao.tan@outlook.com>
2021-05-05 20:33:24 -0700

    more edge cases updated

--- fetcher.js
+++ fetcher.js
@@ -3,44 +3,45 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 const URL = args[0];
 const pathToStore = args[1];
+const sizeOf = require('object-sizeof')
 
 request(URL, (error, response, body) => {
-  console.log('error:', error); 
   if (error) {
-    return;
+    return  console.log('error:', error); 
   }
   console.log('statusCode:', response && response.statusCode); 
   if(response.statusCode !== 200) {
     return;
   }
-  // console.log('body:', body);
-  fs.writeFile(pathToStore, body, () => {
-    if (!error) {
-      console.log("Successfully downloaded!");
-    }
-  });
+
+  fs.readFile(pathToStore, 'utf8', (error) => {
+    console.log("error", error);
+    if (error){
+      fs.writeFile(pathToStore, body, () => {
+        console.log(`Successfully downloaded! File size: ${sizeOf(body)} bytes to ${pathToStore}!`); 
+    });
+    } else {
+      const readline = require('readline');
+      const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+      rl.question("File aready exits, do you want to overwrite? (Y): ", (answer) => {
+        if (answer !== 'Y' && answer !== 'y') {
+          rl.close();
+          return console.log("Did nothing!");
+        } else {
+          fs.writeFile(pathToStore, body, (error) => {
+            if (!error) {
+              console.log(`Successfully downloaded! File size: ${sizeOf(body)} bytes to ${pathToStore}!`);
+            }
+          });
+        }
+        rl.close();
+      })
+    } 
+  })
 });
 
 
 
-/* fs.readFile(pathToStore, 'utf8', () => {
-  const readline = require('readline');
-  const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-  if (error) {
-    rl.question("File aready exits, do you want to overwrite? (Y): ", (answer) => {
-      if (answer !== 'Y' && answer !== 'y') {
-        rl.cloce();
-        return;
-      }
-    })
-  }
-
-  fs.writeFile(pathToStore, body, () => {
-    if (!error) {
-      console.log("Successfully downloaded!");
-    }
-  });
-}) */
